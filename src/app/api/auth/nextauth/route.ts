0d9f5b3a1168179a140875069819c05ced00e633
Pedro Vicente Marcano Perez <72564682+pmarcano2@users.noreply.github.com>
2026-07-05 14:16:22 -0400

    fix: remove incorrectly named nextauth route

--- src/app/api/auth/nextauth/route.ts
+++
@@ -1,7 +0,0 @@
-// [...nextauth]/route.ts
-// Rename this folder to [...nextauth] in your local repo
-import NextAuth from 'next-auth'
-import { authOptions } from '@/lib/auth/config'
-
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
