ca1ac50085af002b26d982cd991f186ff4900df5
Witmakers Agent <pedro@witmakers.com>
2026-09-09 13:25:55 +0000

    feat: bio de Mariela añade 'amiga' (esposa, hija, hermana y amiga)

--- src/components/landing/BioSection.tsx
+++ src/components/landing/BioSection.tsx
@@ -34,7 +34,7 @@ export function BioSection() {
         {/* Biografia */}
         <div className="max-w-3xl mx-auto space-y-6 font-arimo text-ink/70 text-base leading-relaxed">
           <p>
-            ¡Hol@! 💜 Soy Mariela Barbetti, venezolana, inmigrante en EE. UU. desde 2015, madre de cuatro hijos (Paul, Abraham, Alena y Ana), esposa, hija y hermana.
+            ¡Hol@! 💜 Soy Mariela Barbetti, venezolana, inmigrante en EE. UU. desde 2015, madre de cuatro hijos (Paul, Abraham, Alena y Ana), esposa, hija, hermana y amiga.
           </p>
 
           <p>
